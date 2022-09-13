130842ca6fd1700a6d6fc3988f45f284daa2d628
Mahmut Edip Negiz <100766962+medipnegiz@users.noreply.github.com>
2022-09-13 08:55:58 +0200

    phonebook application uploaded

---
+++ Terraform_Phonebook_Application_deployed_on_AWS/user-data.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+yum update -y
+yum install python3 -y
+pip3 install flask
+pip3 install flask_mysql
+yum install git -y
+TOKEN="<TOKEN HERE>" # dont forget
+cd /home/ec2-user
+git clone https://$TOKEN@github.com/medipnegiz/phonebook.git
+python3 /home/ec2-user/phonebook/phonebook-app.py
